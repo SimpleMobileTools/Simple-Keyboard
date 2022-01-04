6c97150caa67e000e666a73d7bd4da19056515a7
tibbi <tibor@kaputa.sk>
2022-01-04 12:25:41 +0100

    create SimpleActivity

--- app/src/main/kotlin/com/simplemobiletools/keyboard/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/keyboard/activities/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.simplemobiletools.keyboard.activities
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.simplemobiletools.commons.extensions.appLaunched
+import com.simplemobiletools.keyboard.BuildConfig
 import com.simplemobiletools.keyboard.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : SimpleActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        appLaunched(BuildConfig.APPLICATION_ID)
     }
 }
