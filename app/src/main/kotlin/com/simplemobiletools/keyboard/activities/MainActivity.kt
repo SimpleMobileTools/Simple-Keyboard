f60658b41295c72666beca29d197508ca158a95b
tibbi <tibor@kaputa.sk>
2022-01-04 12:21:44 +0100

    updating the app icon

---
+++ app/src/main/kotlin/com/simplemobiletools/keyboard/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.simplemobiletools.keyboard.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.simplemobiletools.keyboard.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
