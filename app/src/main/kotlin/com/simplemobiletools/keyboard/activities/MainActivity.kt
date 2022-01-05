fe8e44b162dda0fc713fbb5b8a77e66a32b30ba8
tibbi <tibor@kaputa.sk>
2022-01-05 09:48:41 +0100

    adding FAQ and Settings activities

--- app/src/main/kotlin/com/simplemobiletools/keyboard/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/keyboard/activities/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.simplemobiletools.keyboard.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.simplemobiletools.commons.extensions.appLaunched
+import com.simplemobiletools.commons.models.FAQItem
 import com.simplemobiletools.keyboard.BuildConfig
 import com.simplemobiletools.keyboard.R
 
@@ -11,5 +15,30 @@ class MainActivity : SimpleActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         appLaunched(BuildConfig.APPLICATION_ID)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> startActivity(Intent(applicationContext, SettingsActivity::class.java))
+            R.id.about -> launchAbout()
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
+    private fun launchAbout() {
+        val licenses = 0
+
+        val faqItems = arrayListOf(
+            FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
+            FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons),
+        )
+
+        startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
     }
 }
