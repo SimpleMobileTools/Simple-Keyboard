a9b409a5aa37e0a8f0ca993c0aac0b8511b11dc9
tibbi <tibor@kaputa.sk>
2022-07-06 18:31:58 +0200

    tweaking the edit dialog with Material theme

--- app/src/main/kotlin/com/simplemobiletools/keyboard/dialogs/AddOrEditClipDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/keyboard/dialogs/AddOrEditClipDialog.kt
@@ -1,19 +1,25 @@
 package com.simplemobiletools.keyboard.dialogs
 
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatEditText
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
 import com.simplemobiletools.commons.extensions.*
 import com.simplemobiletools.commons.helpers.ensureBackgroundThread
 import com.simplemobiletools.keyboard.R
 import com.simplemobiletools.keyboard.helpers.ClipsHelper
 import com.simplemobiletools.keyboard.models.Clip
-import kotlinx.android.synthetic.main.dialog_add_or_edit_clip.view.*
 
 class AddOrEditClipDialog(val activity: BaseSimpleActivity, val originalClip: Clip?, val callback: () -> Unit) {
     init {
-        val view = activity.layoutInflater.inflate(R.layout.dialog_add_or_edit_clip, null).apply {
+        val layoutId = if (activity.baseConfig.isUsingSystemTheme) {
+            R.layout.dialog_add_or_edit_clip_material
+        } else {
+            R.layout.dialog_add_or_edit_clip
+        }
+
+        val view = activity.layoutInflater.inflate(layoutId, null).apply {
             if (originalClip != null) {
-                add_clip_value.setText(originalClip.value)
+                findViewById<AppCompatEditText>(R.id.add_clip_value).setText(originalClip.value)
             }
         }
 
@@ -22,9 +28,9 @@ class AddOrEditClipDialog(val activity: BaseSimpleActivity, val originalClip: Cl
             .setNegativeButton(R.string.cancel, null)
             .apply {
                 activity.setupDialogStuff(view, this) { alertDialog ->
-                    alertDialog.showKeyboard(view.add_clip_value)
+                    alertDialog.showKeyboard(view.findViewById(R.id.add_clip_value))
                     alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-                        val clipValue = view.add_clip_value.value
+                        val clipValue = view.findViewById<AppCompatEditText>(R.id.add_clip_value).value
                         if (clipValue.isEmpty()) {
                             activity.toast(R.string.value_cannot_be_empty)
                             return@setOnClickListener
