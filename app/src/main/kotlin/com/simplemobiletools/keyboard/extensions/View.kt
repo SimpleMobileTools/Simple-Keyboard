0035e6440c4b86ad58b9f1689213d1fa0a0dc6b4
Tibor Kaputa <tibor@kaputa.sk>
2022-07-31 16:56:53 +0200

    removing a useless comment + style cleanup

--- app/src/main/kotlin/com/simplemobiletools/keyboard/extensions/View.kt
+++ app/src/main/kotlin/com/simplemobiletools/keyboard/extensions/View.kt
@@ -2,16 +2,9 @@ package com.simplemobiletools.keyboard.extensions
 
 import androidx.recyclerview.widget.RecyclerView
 
-/**
- * Calls the [scroll] callback when the receiving RecyclerView's scroll position is changed.
- */
 fun RecyclerView.onScroll(scroll: (Int) -> Unit) {
     addOnScrollListener(object : RecyclerView.OnScrollListener() {
-        override fun onScrolled(
-            recyclerView: RecyclerView,
-            dx: Int,
-            dy: Int,
-        ) {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
             super.onScrolled(recyclerView, dx, dy)
             scroll(computeVerticalScrollOffset())
         }
