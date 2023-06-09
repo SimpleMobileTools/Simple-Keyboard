09bc110eb6b49980419bcd3debdec99b1191ba44
merkost <konstantinlikes@gmail.com>
2023-06-09 12:10:00 +1000

    Fixed a bug when clicking on layout change and space at the same time

--- app/src/main/kotlin/com/simplemobiletools/keyboard/views/MyKeyboardView.kt
+++ app/src/main/kotlin/com/simplemobiletools/keyboard/views/MyKeyboardView.kt
@@ -1154,8 +1154,8 @@ class MyKeyboardView @JvmOverloads constructor(context: Context, attrs: Attribut
 
                 // fix a glitch with long pressing backspace, then clicking some letter
                 if (mRepeatKeyIndex != NOT_A_KEY) {
-                    val key = mKeys[mRepeatKeyIndex]
-                    if (key.code == KEYCODE_DELETE) {
+                    val key = mKeys.getOrNull(mRepeatKeyIndex)
+                    if (key?.code == KEYCODE_DELETE) {
                         mHandler?.removeMessages(MSG_REPEAT)
                         mRepeatKeyIndex = NOT_A_KEY
                     }
