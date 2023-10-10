0bba7401377879d1db97d09279201778e2b5f94c
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2023-10-10 18:28:15 +0300

    Don't trim the text when inserting into local DB via 'Manage clipboard items'. But value is also trimmed in the EditText extension from Simple-Commons.

--- app/src/main/kotlin/com/simplemobiletools/keyboard/helpers/ClipsHelper.kt
+++ app/src/main/kotlin/com/simplemobiletools/keyboard/helpers/ClipsHelper.kt
@@ -8,7 +8,6 @@ class ClipsHelper(val context: Context) {
 
     // make sure clips have unique values
     fun insertClip(clip: Clip): Long {
-        clip.value = clip.value.trim()
         return if (context.clipsDB.getClipWithValue(clip.value) == null) {
             context.clipsDB.insertOrUpdate(clip)
         } else {
